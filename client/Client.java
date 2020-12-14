d70e8ad0f0bc8c55eaf8adfaae200ca32146724c
Wiktus <68346492+WiktorSa@users.noreply.github.com>
2020-12-14 09:12:29 +0100

    Add files via upload

--- client/Client.java
+++ client/Client.java
@@ -1,4 +1,7 @@
 package client;
+
+import java.util.Scanner;
+
 public class Client
 {
 	private String firstName;
@@ -57,5 +60,103 @@ public class Client
 	public String toString() 
 	{
 		return "Dane o kliencie: Imie: " + firstName + ", Nazwisko: " + lastName + ", Email: " + email + ", Numer telefonu: " + phoneNumber;
+	}
+
+	@SuppressWarnings("resource")
+	public void setClientInfo()
+	{
+		System.out.println("Podaj imie");
+		setCorrectFirstName();
+		System.out.println("Podaj nazwisko");
+		setCorrectLastName();
+		System.out.println("Podaj adres email");
+		setCorrectEmail();
+		System.out.println("Podaj numer telefonu");
+		setCorrectPhoneNumber();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(toString());
+		System.out.println("Nacisnij 1 zeby potwierdzic swoje dane");
+		String decision = scanner.nextLine();
+		if (!decision.equals("1")){
+			setClientInfo();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	private void setCorrectFirstName()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String firstName = scanner.nextLine();
+		boolean isCorrectData = true;
+		// Sprawdzam czy nie ma zadnych dziwnych znakow w imieniu
+		char[] letters = firstName.toCharArray();
+		for (int i=0; i<letters.length; i++)
+		{
+			if (!Character.isLetter(letters[i])) {
+				System.out.println("Podaj poprawne imie");
+				isCorrectData = false;
+				break;
+			}
+		}
+		if (isCorrectData) {
+			this.firstName = firstName;
+		}
+		else {
+			setCorrectFirstName();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	private void setCorrectLastName()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String lastName = scanner.nextLine();
+		boolean isCorrectData = true;
+		// Sprawdzam czy nie ma zadnych dziwnych znakow w imieniu
+		char[] letters = lastName.toCharArray();
+		for (int i=0; i<letters.length; i++)
+		{
+			if (!Character.isLetter(letters[i])) {
+				System.out.println("Podaj poprawne nazwisko");
+				isCorrectData = false;
+				break;
+			}
+		}
+		if (isCorrectData) {
+			this.lastName = lastName;
+		}
+		else {
+			setCorrectLastName();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	private void setCorrectEmail()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String email = scanner.nextLine();
+		// Sprawdzam czy mail jest poprawny
+		String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		if (email.matches(regex)) {
+			this.email = email;
+		}
+		else {
+			System.out.println("Podaj poprawny adres email");
+			setCorrectEmail();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	private void setCorrectPhoneNumber()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String phoneNumber = scanner.nextLine();
+		if (phoneNumber.length() == 9) {
+			this.phoneNumber = phoneNumber;
+		}
+		else {
+			System.out.println("Podaj poprawny numer telefonu");
+			setCorrectPhoneNumber();
+		}
 	}
 }
