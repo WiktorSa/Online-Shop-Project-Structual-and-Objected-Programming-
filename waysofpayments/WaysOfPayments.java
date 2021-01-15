05554542bc27aa3b2e95b1839df65671078e7020
SzymonSawczuk <75539384+SzymonSawczuk@users.noreply.github.com>
2021-01-15 14:40:43 +0100

    Add files via upload

--- waysofpayments/WaysOfPayments.java
+++ waysofpayments/WaysOfPayments.java
@@ -1,5 +1,8 @@
 package waysofpayments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import client.Client;
 
 //Interfejs zaimplementowany przez Szymona Sawczuka
@@ -9,5 +12,6 @@ public interface WaysOfPayments{
 	public abstract String getName();
 	public abstract boolean isPaymentDone();
 	public abstract void pay(Client client); //NOTE: Metoda dokonywania platnosci
+	public final ArrayList<String> TYPESOFPAYMENT = new ArrayList<String>(Arrays.asList("Blik","Platnosc karta","Paypal"));
 
 }
