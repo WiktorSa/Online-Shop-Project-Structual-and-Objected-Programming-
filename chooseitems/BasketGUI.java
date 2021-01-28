4f1cbb7b50f41558cf30affa04e5ed5b9d2c2534
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2021-01-28 11:42:36 +0100

    Add files via upload

---
+++ chooseitems/BasketGUI.java
@@ -0,0 +1,130 @@
+package guiChooseItems;
+
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import chooseitems.Product;
+import client.Client;
+import guiShop.MainGUI;
+import guiClient.ClientSetClientInfoGUI;
+
+//Klasa stworzona przez Wiktora Sadowego
+public class BasketGUI
+{
+	private Client client;
+	private JPanel jPanel;
+	// Bedziemy przekazywac informacje o produkcie i jego ilosci potem, zeby klient mogl skasowac produkty z koszyka
+	private HashMap<JButton, Product> eraseProductButtons = new LinkedHashMap<JButton, Product>();
+	private JButton giveBasicInfoJButton;
+	private MainGUI main;
+	private ClientSetClientInfoGUI setInfoCategory;
+	private JPanel setInfoPanel;
+	
+	public JPanel getJPanel() {
+		return jPanel;
+	}
+	
+	public BasketGUI(MainGUI main)
+	{
+		this.main=main;
+		client =main.getClient();
+
+		jPanel = new JPanel();
+		jPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 7;
+		gbc.gridheight = 3;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		
+		JLabel titileJLabel = new JLabel("Zawartosc koszyka");
+		titileJLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		titileJLabel.setFont(new Font("New Times Roman", Font.BOLD, 40));
+		jPanel.add(titileJLabel, gbc);
+		
+		gbc.gridwidth = 5;
+		gbc.gridy = 3;
+		
+		// Zdobywamy liste produktow, zeby moc ja wypisac na ekran
+		Iterator<Entry<Product, Integer>> listOfBoughtItems = this.client.getBasket().getProducts().entrySet().iterator();
+		
+		while(listOfBoughtItems.hasNext())
+		{
+			Entry<Product, Integer> entry = listOfBoughtItems.next();
+			
+			JLabel jLabel = new JLabel(entry.getKey().toStringOneLine() + " Lista produktow: " + entry.getValue());
+			jLabel.setBorder(new EmptyBorder(0, 10, 0, 5));
+			jPanel.add(jLabel, gbc);
+			
+			gbc.gridx = 5;
+			gbc.gridwidth = 2;
+			
+			JButton jButton = new JButton("Skasuj produkt z koszyka");
+			jButton.addActionListener(new EraseItems());
+			eraseProductButtons.put(jButton, entry.getKey());
+			jPanel.add(jButton, gbc);
+			
+			gbc.gridx = 0;
+			gbc.gridwidth = 5;
+			gbc.gridy += 3;
+		}
+		
+		gbc.gridwidth = 7;
+		
+		JLabel priceJLabel = new JLabel("Cena koncowa: " + String.format("%.2f", this.client.getBasket().getPrice()) + " zl");
+		priceJLabel.setBorder(new EmptyBorder(15, 10, 15, 10));
+		priceJLabel.setFont(new Font("New Times Roman", Font.BOLD, 27));
+		jPanel.add(priceJLabel, gbc);
+		
+		gbc.gridy += 3;
+		gbc.gridheight = 1;
+		
+		giveBasicInfoJButton = new JButton("Zakoncz transakcje");
+		giveBasicInfoJButton.addActionListener(new GiveBasicInfo());
+		jPanel.add(giveBasicInfoJButton, gbc);
+	
+	}
+	
+	class EraseItems implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event) 
+		{
+			for (JButton jButton : eraseProductButtons.keySet()) 
+			{
+				if (event.getSource() == jButton) {
+					// Biore klienta, produkt z HashMapa oraz ilosc danego przedmiotu z koszyka klienta
+					new EraseItemGUI(client, eraseProductButtons.get(jButton), client.getBasket().getProducts().get(eraseProductButtons.get(jButton)));
+				
+				}
+			}
+		}
+	}
+	
+	class GiveBasicInfo implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event) 
+		{
+			setInfoCategory=new ClientSetClientInfoGUI(main);
+			setInfoPanel=setInfoCategory.getjPanel();
+			main.getCardPanel().add(setInfoPanel,"Delivery Page");
+			main.getCardLayout().show(main.getCardPanel(), "Delivery Page");
+		}
+	}
+}
