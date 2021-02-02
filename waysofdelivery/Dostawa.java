60714125cc7c879492d6d835d40a5ddb74856f1c
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2021-02-02 09:39:27 +0100

    Revert "Add files via upload"
    This reverts commit 1677e8d52cdfc82b9c2c3fa7c4cd7fc8972684e4.

--- waysofdelivery/Dostawa.java
+++ waysofdelivery/Dostawa.java
@@ -1,7 +1,10 @@
 package waysofdelivery;
 
+import java.util.ArrayList;
+
 // Interfejs stworzony przez Jana Skibiñskiego
 public interface Dostawa {
 	public String toString();
-	public void changeLayout();
+	public boolean isCorrectData(ArrayList<String> arrayList);
+	public void setDeliveryInfo(ArrayList<String> arrayList);
 }
