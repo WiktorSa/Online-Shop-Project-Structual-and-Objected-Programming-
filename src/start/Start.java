ef4ba922b18be1f0a290f3b878d7bdc58ff44c4a
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2022-01-25 18:51:54 +0100

    start refactor

--- src/start/Start.java
+++ src/start/Start.java
@@ -1,9 +1,7 @@
 package start;
 
 import java.awt.Font;
-
 import javax.swing.UIManager;
-
 import client.Client;
 import client.UnregisteredClient;
 import gui.shop.MainGUI;
@@ -11,12 +9,11 @@ import gui.shop.MainGUI;
 public class Start {
 	final static String LOOKANDFEEL = "Metal";
 	final static String THEME = "Ocean";
-	// Set default font for the whole programme
+	// Set default font for the whole program
 	public static void setUIFont (javax.swing.plaf.FontUIResource f) {
 	    @SuppressWarnings("rawtypes")
 		java.util.Enumeration keys = UIManager.getDefaults().keys();
-	    while (keys.hasMoreElements()) 
-	    {
+	    while (keys.hasMoreElements()) {
 	      Object key = keys.nextElement();
 	      Object value = UIManager.get (key);
 	      if (value instanceof javax.swing.plaf.FontUIResource) {
@@ -27,12 +24,9 @@ public class Start {
 	
 	public static void main(String[] args) {
 		setUIFont (new javax.swing.plaf.FontUIResource("Dialog",Font.PLAIN,14));
-		try 
-		{
+		try {
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel"); 
-		}
-		catch(Exception e) 
-		{
+		} catch(Exception e) {
 			System.exit(-1);
 		}
 		// Client is not registered at the beginning
